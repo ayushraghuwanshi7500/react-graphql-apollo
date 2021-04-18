6edac6649f7415d3a4b49f5263ec2fd99e44f639
ayushraghuwanshi7500 <66081125+ayushraghuwanshi7500@users.noreply.github.com>
2021-04-18 14:25:14 +0530

    filter

--- client/src/components/Launches.jsx
+++ client/src/components/Launches.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { gql, useQuery } from '@apollo/client';
 import LaunchesItem from './LaunchesItem';
+import MissionKey from './MissionKey';
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery {
     launches {
@@ -14,14 +15,28 @@ const LAUNCHES_QUERY = gql`
 
 const Launches = () => {
   const { loading, error, data } = useQuery(LAUNCHES_QUERY);
-
+  const [filter, setFilter] = useState('all');
+  const handleFilter = (e) => {
+    setFilter(e.target.value);
+  };
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
   return (
     <div>
       <h1 className='display-4 my-3'>Launches</h1>
+      <MissionKey />
+      <div>
+        <select onChange={handleFilter}>
+          <option value='all' selected>
+            all
+          </option>
+          <option value='success'>Success</option>
+          <option value='fail'>Fail</option>
+        </select>
+      </div>
       {data.launches.map((curr) => (
         <LaunchesItem
+          filter={filter}
           key={curr.flight_number}
           flight_number={curr.flight_number}
           mission_name={curr.mission_name}
--- client/src/components/LaunchesItem.jsx
+++ client/src/components/LaunchesItem.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
-
+import classNames from 'classnames';
 const LaunchesItem = ({
+  filter,
   flight_number,
   mission_name,
   launch_date_local,
   launch_success
 }) => {
-  return (
+  return filter == 'all' ? (
     <div className='card card-body mb-3'>
       <div className='row'>
         <div className='col-md-9'>
-          <h4>Mission: {mission_name}</h4>
+          <h4>
+            Mission:
+            <span
+              className={classNames({
+                'text-success': launch_success,
+                'text-danger': !launch_success
+              })}
+            >
+              {mission_name}
+            </span>
+          </h4>
           <p>Date: {launch_date_local}</p>
         </div>
         <div className='col-md-3'>
@@ -18,7 +29,51 @@ const LaunchesItem = ({
         </div>
       </div>
     </div>
-  );
+  ) : filter === 'success' && launch_success ? (
+    <div className='card card-body mb-3'>
+      <div className='row'>
+        <div className='col-md-9'>
+          <h4>
+            Mission:
+            <span
+              className={classNames({
+                'text-success': launch_success,
+                'text-danger': !launch_success
+              })}
+            >
+              {mission_name}
+            </span>
+          </h4>
+          <p>Date: {launch_date_local}</p>
+        </div>
+        <div className='col-md-3'>
+          <button className='btn btn-secondary'> Launch Details </button>
+        </div>
+      </div>
+    </div>
+  ) : filter === 'fail' && !launch_success ? (
+    <div className='card card-body mb-3'>
+      <div className='row'>
+        <div className='col-md-9'>
+          <h4>
+            Mission:
+            <span
+              className={classNames({
+                'text-success': launch_success,
+                'text-danger': !launch_success
+              })}
+            >
+              {mission_name}
+            </span>
+          </h4>
+          <p>Date: {launch_date_local}</p>
+        </div>
+        <div className='col-md-3'>
+          <button className='btn btn-secondary'> Launch Details </button>
+        </div>
+      </div>
+    </div>
+  ) : null;
 };
 
 export default LaunchesItem;
